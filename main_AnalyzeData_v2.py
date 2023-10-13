fixed average curve dimensional dimension bug

main_AnalyzeData_v2.py
@@ -93,11 +93,41 @@ if Data_Type == 'dict':
     # ExpData = ExpData_new
     # Keys = list( ExpData.keys() )
 
-
 elif Data_Type == 'raw':
     DataFile = exp_files + '\\%s\\%s 1 S-1 X-1 Y-1 I-1.txt'%(DirName,FileName)
     ExpData  = AnalyzeExpData(exp_files,DirName,FileName, Xs, Ys, Material_Width, Material_Thick)
     Keys     = list( ExpData.keys() )
+
+
+
+
+
+ExpData_2 = {}
+for n in range(50):
+    ExpData_2[Keys[n]] = ExpData[Keys[n]]
+
+ExpData = ExpData_2
+
+
+
+# # Fit = np.copy(Fits_Lsq_NH)
+# Fit = np.copy(Fits_DirInv_NH)
+
+# N = 50
+# Nd = 100
+# fig = plt.figure(1,figsize=(8,8))
+# plt.plot( ExpData[Keys[N]]['Indentation'][::5]/ExpData[Keys[N]]['radius'], ExpData[Keys[N]]['Load'][::5],'ok',label='Experimental Data')
+# plt.plot(np.linspace(0,0.5,Nd+1)[1:], Fit[N,:],color='navy',linewidth=3,label='ML Prediction: $\mu$=%5.2f, Jm=%5.3f'%(Exp_Params_DirInv_GT[N,0],Exp_Params_DirInv_GT[N,1]),alpha=0.5)
+
+
+
+
+
+
+
+
+
+
 
 
 

subfunctions_Analyze_v2.py
@@ -91,10 +91,11 @@ def ExtractData_HertzFits(ExpData, Keys):
         
     # Averaged Experimental Data
     ExpData_Load_Avg = np.average(ExpData_Load_Fit,axis=0)
-    ExpData_W_Avg    = np.average(ExpData_Dim[:,0])*ExpData[Keys[0]]['radius']
-    ExpData_H_Avg    = np.average(ExpData_Dim[:,1])*ExpData[Keys[0]]['radius']
+    ExpData_W_Avg    = np.average(ExpData_Dim[:,0])#*ExpData[Keys[0]]['radius']
+    ExpData_H_Avg    = np.average(ExpData_Dim[:,1])#*ExpData[Keys[0]]['radius']
     ExpData_R_Avg    = np.average(ExpData_Rad[:])
     ExpData['Averaged'] = {'radius':ExpData_R_Avg, 'Height':ExpData_H_Avg, 'Width':ExpData_W_Avg, 'Load':ExpData_Load_Avg, 'Indentation': xi*ExpData_R_Avg}
+    
     
     # include averaged data
     Keys = list(ExpData.keys())
@@ -745,7 +746,7 @@ def LeastSquaresPrediction_NH(Thresh, syn_files, model_Gent_Forward, Exp_Params_
             
             try:
                 Lsq_params, pcov_neww = optimize.curve_fit(LSQ_Func, LSQ_Disp,LSQ_Load,p0=(x0[0], x0[1]), 
-                                                           bounds = ([-0.25,1.0],[1.25,1.1]), ftol = 1e-15, xtol = 1e-15,maxfev=200)
+                                                           bounds = ([-0.25,0.99999],[1.25,1.0000001]), ftol = 1e-15, xtol = 1e-15,maxfev=200)
             except RuntimeError:
                 print("Error - curve_fit failed")
                         
@@ -769,7 +770,6 @@ def LeastSquaresPrediction_NH(Thresh, syn_files, model_Gent_Forward, Exp_Params_
 
             
     return Exp_Params_DirInv_GT_Cl, Fits_DirInv_Lsq_Fix
-
 
 
 